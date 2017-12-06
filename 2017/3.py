f09fcc595ce3885df12f044e48982064816b3227
Josh Smith <jsmith@pointmarc.com>
2017-12-05 18:31:47 -0600

    finished two, need an ulam spiral gen to finish three

---
+++ 2017/3.py
@@ -0,0 +1,15 @@
+for i in range(101,1000,2):
+    print i,i*i
+    if i*i > 312051:
+        break
+
+
+print 559/2
+
+# 558 steps from lower right corner
+
+print 312481 - 312051
+
+print 430-279
+
+print 151+279
